bd6779c3c055431b53f0d0e4fc8b3efdc3cb0075
jeverlierosario <jeverlierosario@outlook.com>
2023-10-14 23:23:19 +0100

    fixed shows

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -7,43 +7,59 @@ import {BandSiteAPI} from "./band-site-api.js";
 const APIKey = "ad84ec4e-4017-4519-9ec4-26eef3e0327c";
 const api = new BandSiteAPI(APIKey);
 
-const comments = await api.getShows();
+const concerts = await api.getShows();
+console.log(concerts);
 
-const concerts = [
-  {
-    date: "Mon Sept 06 2021",
-    venue: "Ronald Lane",
-    location: "San Francisco, CA"
-  },
-  {
-    date: "Tue Sept 21 2021",
-    venue: "Pier 3 East",
-    location: "San Francisco, CA"
-  },
-  {
-    date: "Fri Oct 15 2021",
-    venue: "View Lounge",
-    location: "San Francisco, CA"
-  },
-  {
-    date: "Sat Nov 06 2021",
-    venue: "Hyatt Agency",
-    location: "San Francisco, CA"
-  },
-  {
-    date: "Fri Nov 26 2021",
-    venue: "Moscow Center",
-    location: "San Francisco, CA"
-  },
-  {
-    date: "Wed Dec 15 2021",
-    venue: "Press Club",
-    location: "San Francisco, CA"
-  }
-];
+// const concerts = [
+//   {
+//     date: "Mon Sept 06 2021",
+//     venue: "Ronald Lane",
+//     location: "San Francisco, CA"
+//   },
+//   {
+//     date: "Tue Sept 21 2021",
+//     venue: "Pier 3 East",
+//     location: "San Francisco, CA"
+//   },
+//   {
+//     date: "Fri Oct 15 2021",
+//     venue: "View Lounge",
+//     location: "San Francisco, CA"
+//   },
+//   {
+//     date: "Sat Nov 06 2021",
+//     venue: "Hyatt Agency",
+//     location: "San Francisco, CA"
+//   },
+//   {
+//     date: "Fri Nov 26 2021",
+//     venue: "Moscow Center",
+//     location: "San Francisco, CA"
+//   },
+//   {
+//     date: "Wed Dec 15 2021",
+//     venue: "Press Club",
+//     location: "San Francisco, CA"
+//   }
+// ];
+
+
+
+
 
 function renderConcertList() {
   const concertListContainer = document.getElementById("concertList");
+
+  function formatDate(inputDate) {
+    const options = { weekday: 'short', month: 'short', day: '2-digit', year: 'numeric' };
+    const date = new Date(inputDate);
+    return date.toLocaleDateString('en-US', options);
+  }
+
+
+
+
+
 
 
   concerts.forEach(concert => {
@@ -55,13 +71,14 @@ function renderConcertList() {
     dateElement.classList.add("concert__list--date");
     dateElement.innerText = "Date";
     const dateValue = document.createElement("p");
-    dateValue.innerText = concert.date;
+    dateValue.innerText = formatDate(concert.date);
 
     const venueElement = document.createElement("h3");
     venueElement.classList.add("concert__list--venue");
     venueElement.innerText = "Venue";
     const venueValue = document.createElement("p");
-    venueValue.innerText = concert.venue;
+    venueValue.innerText = concert.place;
+   console.log(typeof venueValue);
 
     const locationElement = document.createElement("h3");
     locationElement.classList.add("concert__list--location");
